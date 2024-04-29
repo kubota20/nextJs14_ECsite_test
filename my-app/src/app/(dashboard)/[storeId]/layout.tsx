7eb3cfddb0000090a4ef0afbbdfe737e9ce53ec8
kubota20 <doc0000doc77@gmail.com>
2024-04-29 16:13:42 +0900

    NavBarの追加

--- my-app/src/app/(dashboard)/[storeId]/layout.tsx
+++ my-app/src/app/(dashboard)/[storeId]/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
@@ -31,7 +32,7 @@ export default async function AuthLayout({
 
   return (
     <>
-      <div>ここはNavbarを入れる</div>
+      <Navbar />
       {children}
     </>
   );
---
+++ my-app/src/components/main-nav.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+
+export function MainNav({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  const pathName = usePathname();
+  const params = useParams();
+
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: "settings",
+      active: pathName === `/${params.storeId}/setting`,
+    },
+  ];
+  return (
+    <nav className={cn("flex item-center space-x-4 lg:space-x-6", className)}>
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            route.active
+              ? "text-black derk:text-white"
+              : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+}
