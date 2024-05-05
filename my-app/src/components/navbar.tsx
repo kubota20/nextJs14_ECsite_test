4e3c2e1f29cb73466a8a6c5add1dc15b8fcf1360
kubota20 <doc0000doc77@gmail.com>
2024-05-05 16:10:10 +0900

    @clerk/nextjs v5になった為書き方が少し変更

--- my-app/src/components/navbar.tsx
+++ my-app/src/components/navbar.tsx
@@ -1,4 +1,5 @@
-import { UserButton, auth } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { MainNav } from "./main-nav";
 import StoreSwitcher from "./store-switcher";
 import { redirect } from "next/navigation";
