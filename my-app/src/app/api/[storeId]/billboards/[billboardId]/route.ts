715483120e0db85b5443dd93b442d3f51ca49808
kubota20 <doc0000doc77@gmail.com>
2024-05-08 13:10:44 +0900

    billboardのAPI route を作成

---
+++ my-app/src/app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -0,0 +1,119 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; billboardId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+
+    // ユーザIDが問題がある場合
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    // ユーザー名が問題がある場合
+    if (!label) {
+      return new NextResponse("label is required", { status: 400 });
+    }
+
+    // ユーザー名が問題がある場合
+    if (!imageUrl) {
+      return new NextResponse("image URL is required", { status: 400 });
+    }
+
+    // storeIdに問題がある場合
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    // billboardIdに問題がある場合
+    if (!params.billboardId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    // findFirst 取得 条件に一致する最初のレコードを取得
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    // id 問題がある場合
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    // updateMany 複数件更新 条件に一致する全てのレコードを更新
+    const billboard = await prismadb.billboard.updateMany({
+      where: {
+        id: params.billboardId,
+      },
+      data: {
+        label,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string; billboardId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    // ユーザIDが問題がある場合
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    // storeIdに問題がある場合
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    // billboardIdに問題がある場合
+    if (!params.billboardId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    // findFirst 取得 条件に一致する最初のレコードを取得
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    // id 問題がある場合
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    // prismaで作ったテーブルを持ってきてます
+    // deleteMany 複数件削除 条件に一致する全てのレコードを削除する
+    const billboard = await prismadb.billboard.deleteMany({
+      where: {
+        id: params.billboardId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
