0e9f2c92aeade62dadecbe5b3561da9dcdf3f163
kubota20 <doc0000doc77@gmail.com>
2024-05-08 15:45:50 +0900

    date-fnsの追加とbillboardの取得

--- my-app/src/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ my-app/src/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { BillboardClient } from "./components/client";
+import { format } from "date-fns";
 
-const billboardsPage = () => {
+import prismadb from "@/lib/prismadb";
+import { BillboardClient } from "./components/client";
+import { BillboardColumnProps } from "./components/columns";
+
+const billboardsPage = async ({ params }: { params: { storeId: string } }) => {
+  // findMany	複数件取得 条件に一致する全てのレコードを取得
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      // 順番を決めてます
+      createdAt: "desc",
+    },
+  });
+
+  const formattedBillboardColumn: BillboardColumnProps[] = billboards.map(
+    (item) => ({
+      id: item.id,
+      label: item.label,
+      createdAt: format(item.createdAt, "yyyy年MM月dd日 HH:mm"),
+    })
+  );
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient />
+        <BillboardClient data={formattedBillboardColumn} />
       </div>
     </div>
   );
