deab83327d1cc77ee8f5c2ba4ed4ae23856aae3e
kubota20 <doc0000doc77@gmail.com>
2024-05-12 16:53:07 +0900

    priceを日本の数値書式にします

--- my-app/src/lib/utils.ts
+++ my-app/src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// priceを日本の数値書式にします
+export const formatter = new Intl.NumberFormat("ja-JP", {
+  style: "currency",
+  currency: "JPY",
+});
