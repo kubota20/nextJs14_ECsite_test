e4ee0fc8ae8ca368550083ba70fc94e7fa3b97eb
kubota20 <doc0000doc77@gmail.com>
2024-05-14 11:12:28 +0900

    <div> in <div> のエラー対処

--- ec-store/src/components/main-nav.tsx
+++ ec-store/src/components/main-nav.tsx
@@ -19,20 +19,22 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
   }));
 
   return (
-    <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
-      {routes.map((route) => (
-        <Link
-          key={route.href}
-          href={route.href}
-          className={cn(
-            "text-sm font-medium transition-colors hover:text-black",
-            route.active ? "text-black" : "text-neutral-300"
-          )}
-        >
-          {route.label}
-        </Link>
-      ))}
-    </nav>
+    <>
+      <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
+        {routes.map((route) => (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={cn(
+              "text-sm font-medium transition-colors hover:text-black",
+              route.active ? "text-black" : "text-neutral-300"
+            )}
+          >
+            {route.label}
+          </Link>
+        ))}
+      </nav>
+    </>
   );
 };
 
