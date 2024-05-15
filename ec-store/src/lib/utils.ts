e97e418e425126e0c7c9c6d4954e3edadf8e9ea1
kubota20 <doc0000doc77@gmail.com>
2024-05-15 17:39:22 +0900

    リクエストオプション追加

--- ec-store/src/lib/utils.ts
+++ ec-store/src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// HTTP リクエスト
+export const RequestOptions: RequestInit = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
