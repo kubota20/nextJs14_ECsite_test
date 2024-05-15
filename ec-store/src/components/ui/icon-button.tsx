5d4770cb105949be0147770731d3badd99490985
kubota20 <doc0000doc77@gmail.com>
2024-05-15 18:55:41 +0900

    IconButton作成

---
+++ ec-store/src/components/ui/icon-button.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+interface IconButtonProps {
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  className?: string;
+  icon: React.ReactElement;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({
+  onClick,
+  className,
+  icon,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "rounded-full flex items-center bg-white border shadow-md p-2 hover:scale-110 transition",
+        className
+      )}
+    >
+      {icon}
+    </button>
+  );
+};
+
+export default IconButton;
