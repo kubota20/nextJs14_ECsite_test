cf9104fc05cf3e66c4af264053528a89acbef8c8
kubota20 <doc0000doc77@gmail.com>
2024-05-15 17:41:14 +0900

    追加

--- ec-store/src/app/(routes)/page.tsx
+++ ec-store/src/app/(routes)/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+import { getBillboard } from "@/actions/get-billboard";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+const HomePage = async () => {
+  const billboard = await getBillboard("ec3aaa06-6b5d-4a96-abd3-02c993c93a37");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
