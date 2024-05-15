87caef87769e08f7cca3fbccfd7a90a371703df0
kubota20 <doc0000doc77@gmail.com>
2024-05-15 17:39:34 +0900

    テスト

--- my-app/src/middleware.ts
+++ my-app/src/middleware.ts
@@ -1,14 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/api/(.*)", // すべての API ルートを保護します
-]);
+export default clerkMiddleware({});
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect();
 
-  // 他のルートは保護しません
-});
+//   // 他のルートは保護しません
+// });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
