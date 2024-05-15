d466b30018f860b4fa23d604d2151d554966d0ff
kubota20 <doc0000doc77@gmail.com>
2024-05-15 17:40:16 +0900

    修正

--- ec-store/src/actions/get-categories.tsx
+++ ec-store/src/actions/get-categories.tsx
@@ -1,16 +1,23 @@
 import { Category } from "@/types/types";
+import { RequestOptions } from "@/lib/utils";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 export const getCategories = async (): Promise<Category[]> => {
-  const requestOptions: RequestInit = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+  try {
+    const res = await fetch(URL, {
+      RequestOptions,
+      // revalidate 時間ベースデータ検証 キャッシュの保存時間を3600秒（1時間）に指定
+      next: { revalidate: 3600 },
+    });
 
-  const res = await fetch(URL, requestOptions);
+    if (!res.ok) {
+      throw new Error(`Fetch failed with status ${res.status}`);
+    }
 
-  return res.json();
+    return res.json();
+  } catch (error) {
+    console.error("Failed to fetch categories:", error);
+    throw error;
+  }
 };
