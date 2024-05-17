928dd967d2822e76925bd9f5ad1f973f95d7e45b
kubota20 <doc0000doc77@gmail.com>
2024-05-17 17:15:13 +0900

    info.tsx作成

--- ec-store/src/app/(routes)/product/[productId]/page.tsx
+++ ec-store/src/app/(routes)/product/[productId]/page.tsx
@@ -3,6 +3,7 @@ import { getProducts } from "@/actions/get-products";
 import Container from "@/components/ui/container";
 import Gallery from "@/components/gallery";
 import ProductList from "@/components/product-list";
+import Info from "@/components/ui/info";
 
 interface ProductPageProps {
   params: {
@@ -24,8 +25,10 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
           <div className="px-4 py-10 sm:px-6 lg:px-8">
             <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
               <Gallery images={product?.images} />
-              <div>ギャラリー</div>
-              <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">情報</div>
+
+              <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+                <Info data={product} />
+              </div>
             </div>
             <hr className="my-10" />
             {/* Product List*/}
