2553d26133e681821c50ba4afad358ff4a024cfd
kubota20 <doc0000doc77@gmail.com>
2024-05-17 16:38:12 +0900

    Galleryを作成

--- ec-store/src/app/(routes)/product/[productId]/page.tsx
+++ ec-store/src/app/(routes)/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import { getProduct } from "@/actions/get-product";
 import { getProducts } from "@/actions/get-products";
 import Container from "@/components/ui/container";
+import Gallery from "@/components/gallery";
 import ProductList from "@/components/product-list";
 
 interface ProductPageProps {
@@ -22,10 +23,12 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
         <Container>
           <div className="px-4 py-10 sm:px-6 lg:px-8">
             <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+              <Gallery images={product?.images} />
               <div>ギャラリー</div>
               <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">情報</div>
             </div>
             <hr className="my-10" />
+            {/* Product List*/}
             <ProductList title="アイテム" items={suggestedProducts} />
           </div>
         </Container>
