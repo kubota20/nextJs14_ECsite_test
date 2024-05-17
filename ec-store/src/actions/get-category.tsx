51ee478c988e355c971a35af80d1a4f8b692a121
kubota20 <doc0000doc77@gmail.com>
2024-05-17 21:01:22 +0900

    修正

--- ec-store/src/actions/get-category.tsx
+++ ec-store/src/actions/get-category.tsx
@@ -2,7 +2,7 @@ import { Category } from "@/types/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
-export const getCategory = async (): Promise<Category> => {
+export const getCategory = async (id: string): Promise<Category> => {
   try {
     const res = await fetch(`${URL}/${id}`, {
       next: { revalidate: 0 },
--- ec-store/src/actions/get-products.tsx
+++ ec-store/src/actions/get-products.tsx
@@ -21,7 +21,7 @@ export const getProducts = async (query: Query): Promise<Product[]> => {
     },
   });
   try {
-    const res = await fetch(URL, {
+    const res = await fetch(url, {
       next: { revalidate: 0 },
     });
 
