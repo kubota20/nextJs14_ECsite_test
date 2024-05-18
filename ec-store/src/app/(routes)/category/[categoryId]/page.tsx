00a359d2fbe721cb0b6ea2f7b572de3a5c0d069c
kubota20 <doc0000doc77@gmail.com>
2024-05-18 11:20:28 +0900

    filterとproductCard を追加

--- ec-store/src/app/(routes)/category/[categoryId]/page.tsx
+++ ec-store/src/app/(routes)/category/[categoryId]/page.tsx
@@ -4,12 +4,15 @@ import { getColors } from "@/actions/get-colors";
 import { getCategory } from "@/actions/get-category";
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
+import Filter from "./components/filter";
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
 
 interface CategoryPageProps {
   params: {
     categoryId: string;
   };
-  seartParams: {
+  searchParams: {
     colorId: string;
     sizeId: string;
   };
@@ -17,12 +20,12 @@ interface CategoryPageProps {
 
 const CategoryPage: React.FC<CategoryPageProps> = async ({
   params,
-  seartParams,
+  searchParams,
 }) => {
   const products = await getProducts({
     categoryId: params.categoryId,
-    colorId: seartParams.colorId,
-    sizeId: seartParams.sizeId,
+    colorId: searchParams.colorId,
+    sizeId: searchParams.sizeId,
   });
 
   const sizes = await getSizes();
@@ -33,6 +36,22 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
     <div className="bg-white">
       <Container>
         <Billboard data={category.billboard} />
+        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            <div className="hidden lg:block">
+              <Filter valueKey="sizeId" name="sizes" data={sizes} />
+              <Filter valueKey="colorId" name="colors" data={colors} />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   );
